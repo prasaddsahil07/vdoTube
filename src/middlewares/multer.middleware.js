61f52c4893e24523dcf42f1f48d8375694ec055f
Sahil Prasad <sahilprasad41969@gmailcom>
2025-08-23 02:23:43 +0530

    quick fix

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,15 +1,21 @@
+import fs from "fs";
+import path from "path";
 import multer from "multer";
 
-
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, './public/temp')
-    },
-    filename: function (req, file, cb) {
-        console.log(file)
-        cb(null, file.originalname)
-    }
-    
-  })
+  destination: function (req, file, cb) {
+    const dir = path.join(process.cwd(), "public", "temp");
 
- export const upload = multer({ storage })
+    if (!fs.existsSync(dir)) {
+      fs.mkdirSync(dir, { recursive: true });  // create folder automatically
+    }
+
+    cb(null, dir);
+  },
+  filename: function (req, file, cb) {
+    console.log(file);
+    cb(null, Date.now() + "-" + file.originalname); // avoid overwriting same filenames
+  }
+});
+
+export const upload = multer({ storage });
